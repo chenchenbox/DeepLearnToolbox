9ce48adbcd1c9ffb3f49f1b331cd3f9117c5feaf
張家誠 <bird11419@yahoo.com.tw>
2016-04-30 16:52:53 +0800

    update

--- tests/myAE.m
+++ tests/myAE.m
@@ -4,19 +4,19 @@
 
 	dataset = csvread('C:\Users\chenchen\Orientation\WaveleFeatureSheet\R2\R2.csv');
 
-	h1 = [300, 350, 400, 450, 500];
-	h2 = [80, 120, 140];
-
-	if( isequal(exist('C:\Users\chenchen\Orientation\WaveleFeatureSheet\R2\AE', 'dir'),7) == 0 )
+	h1 = [256, 512, 1024, 2048];
+	h2 = [8, 16, 32, 64, 128];
 	
-		mkdir('C:\Users\chenchen\Orientation\WaveleFeatureSheet\R2\AE');
+	if( isequal(exist('C:\Users\chenchen\Orientation\WaveleFeatureSheet\R2\AE\Fin2Cubic', 'dir'),7) == 0 )
+	
+		mkdir('C:\Users\chenchen\Orientation\WaveleFeatureSheet\R2\AE\Fin2Cubic');
 	
 	end
 	
-	for a1 = 1:5
-		for a2 = 1:3
+	for a1 = 1:size(h1,2)
+		for a2 = 1:size(h2,2)
 
-			mkdir(['C:\Users\chenchen\Orientation\WaveleFeatureSheet\R2\AE\' int2str(h1(a1)) '_' int2str(h2(a2))]);
+			mkdir(['C:\Users\chenchen\Orientation\WaveleFeatureSheet\R2\AE\Fin2Cubic\' int2str(h1(a1)) '_' int2str(h2(a2))]);
 		
 			% Setup and train a stacked denoising autoencoder (SDAE)	
 			rand('state',0)
@@ -30,11 +30,11 @@
 			
 			end
 			
-			opts.numepochs = 2000;
+			opts.numepochs = 1000;
 			opts.batchsize = 24;
 			sae = saetrain(sae, dataset, opts);
 			
-			% SAE fine-tuning
+			% SAE Fin2Cubice-tuning
             nn = nnsetup([200, h1(a1), h2(a2), h1(a1), 200]);    %h3               
             nn.activation_function              = 'sigm';
             nn.learningRate                     = 0.25;
@@ -45,12 +45,12 @@
 				
 			end
 			
-			% fine-tuning之設定
-			opts.numepochs = 2000;
+			% Fin2Cubice-tuning之設定
+			opts.numepochs = 1000;
 			opts.batchsize = 24;
 			
 			% 開始訓練
-			[nn, L, fine_tune_error] = nntrain(nn, dataset, dataset, opts); 
+			[nn, L, Fine_tune_error] = nntrain(nn, dataset, dataset, opts); 
 			
 			% total_batch
 			nn = nnff(nn, dataset, dataset);
@@ -58,33 +58,38 @@
 			%印出權重可加在這邊 start
 			input_layer = nn.a{1};
 			input_layer(:,1)=[];
-			csvwrite(['C:\Users\chenchen\Orientation\WaveleFeatureSheet\R2\AE\' int2str(h1(a1)) '_' int2str(h2(a2)) '\input_layer.csv'],input_layer);
+			csvwrite(['C:\Users\chenchen\Orientation\WaveleFeatureSheet\R2\AE\Fin2Cubic\' int2str(h1(a1)) '_' int2str(h2(a2)) '\input_layer.csv'],input_layer);
 			
 			hidden1_layer = nn.a{2};
 			hidden1_layer(:,1)=[];
-			csvwrite(['C:\Users\chenchen\Orientation\WaveleFeatureSheet\R2\AE\' int2str(h1(a1)) '_' int2str(h2(a2)) '\hidden1_layer.csv'],hidden1_layer);
+			csvwrite(['C:\Users\chenchen\Orientation\WaveleFeatureSheet\R2\AE\Fin2Cubic\' int2str(h1(a1)) '_' int2str(h2(a2)) '\hidden1_layer.csv'],hidden1_layer);
 			
 			hidden2_layer = nn.a{3};
 			hidden2_layer(:,1)=[];
-			csvwrite(['C:\Users\chenchen\Orientation\WaveleFeatureSheet\R2\AE\' int2str(h1(a1)) '_' int2str(h2(a2)) '\hidden2_layer.csv'],hidden2_layer);
+			csvwrite(['C:\Users\chenchen\Orientation\WaveleFeatureSheet\R2\AE\Fin2Cubic\' int2str(h1(a1)) '_' int2str(h2(a2)) '\hidden2_layer.csv'],hidden2_layer);
 			
 			hidden3_layer = nn.a{4};
 			hidden3_layer(:,1)=[];
-			csvwrite(['C:\Users\chenchen\Orientation\WaveleFeatureSheet\R2\AE\' int2str(h1(a1)) '_' int2str(h2(a2)) '\hidden3_layer.csv'],hidden3_layer);
+			csvwrite(['C:\Users\chenchen\Orientation\WaveleFeatureSheet\R2\AE\Fin2Cubic\' int2str(h1(a1)) '_' int2str(h2(a2)) '\hidden3_layer.csv'],hidden3_layer);
 			
 			output_layer = nn.a{5};
-			csvwrite(['C:\Users\chenchen\Orientation\WaveleFeatureSheet\R2\AE\' int2str(h1(a1)) '_' int2str(h2(a2)) '\output_layer.csv'],output_layer);
+			csvwrite(['C:\Users\chenchen\Orientation\WaveleFeatureSheet\R2\AE\Fin2Cubic\' int2str(h1(a1)) '_' int2str(h2(a2)) '\output_layer.csv'],output_layer);
 			%印出權重可加在這邊 end
+			
+			csvwrite(['C:\Users\chenchen\Orientation\WaveleFeatureSheet\R2\AE\Fin2Cubic\' int2str(h1(a1)) '_' int2str(h2(a2)) '\1stLayerMSE.csv'],sae.ae{1}.FullBatchError);
+			csvwrite(['C:\Users\chenchen\Orientation\WaveleFeatureSheet\R2\AE\Fin2Cubic\' int2str(h1(a1)) '_' int2str(h2(a2)) '\2ndLayerMSE.csv'],sae.ae{2}.FullBatchError);
+			csvwrite(['C:\Users\chenchen\Orientation\WaveleFeatureSheet\R2\AE\Fin2Cubic\' int2str(h1(a1)) '_' int2str(h2(a2)) '\3rdLayerMSE.csv'],sae.ae{3}.FullBatchError);
+			csvwrite(['C:\Users\chenchen\Orientation\WaveleFeatureSheet\R2\AE\Fin2Cubic\' int2str(h1(a1)) '_' int2str(h2(a2)) '\FineTuneMSE.csv'],Fine_tune_error);
 			
 			%test
 			[er, bad] = nntest(nn, dataset, dataset);
             disp(['er= ',num2str(er)]);    
 			
 			% 畫Error-rate
-			plot(1:2000,sae.ae{1}.FullBatchError,'-b',1:2000,sae.ae{2}.FullBatchError,'-r',1:2000,sae.ae{3}.FullBatchError,'-g',1:2000,fine_tune_error,'-m');
- 			legend(['AE-' int2str(h1(a1))],['AE-' int2str(h2(a2))],['AE-' int2str(h1(a1))],['fine-tune-error']);
+			plot(1:1000,sae.ae{1}.FullBatchError,'-b',1:1000,sae.ae{2}.FullBatchError,'-r',1:1000,sae.ae{3}.FullBatchError,'-g',1:1000,Fine_tune_error,'-m');
+ 			legend(['AE-' int2str(h1(a1))],['AE-' int2str(h2(a2))],['AE-' int2str(h1(a1))],['Fine-tune-error']);
 			title('MSE-fullBatchError');
-			saveas(gcf,['C:\Users\chenchen\Orientation\WaveleFeatureSheet\R2\AE\' int2str(h1(a1)) '_' int2str(h2(a2)) '\MSE-FullBatchError.png']);
+			saveas(gcf,['C:\Users\chenchen\Orientation\WaveleFeatureSheet\R2\AE\Fin2Cubic\' int2str(h1(a1)) '_' int2str(h2(a2)) '\MSE-FullBatchError.png']);
 	
 		end
 	end
